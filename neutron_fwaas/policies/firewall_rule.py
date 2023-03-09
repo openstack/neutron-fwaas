70d3174f8cdd611e8f9fa94bdce34adbff0fd173
Nurmatov Mamatisa <nurmatov.mamatisa@huawei.com>
2023-03-09 11:37:49 +0300

    Use neutron-lib policy rules
    Change-Id: Iac29a8dd4888eaaf5ff833d840953a5b92bd56e8

--- neutron_fwaas/policies/firewall_rule.py
+++ neutron_fwaas/policies/firewall_rule.py
@@ -10,9 +10,8 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
+from neutron_lib import policy as base
 from oslo_policy import policy
-
-from neutron_fwaas.policies import base
 
 
 rules = [
